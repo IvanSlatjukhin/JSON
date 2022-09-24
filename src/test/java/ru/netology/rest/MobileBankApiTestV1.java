e083cefb62d2956b4146ae415fc09bcf4d362848
IvanSlatjukhin <slatjukhin@mail.ru>
2022-09-24 13:42:17 +0300

    RUR

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,19 +3,24 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
-        // Предусловия
+// Предусловия
         given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
                 .when()
-                .get("/demo/accounts")
-                // Проверки
+                .post("/post")
+// Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
     }
 }
